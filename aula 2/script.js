928c198faf44ff08823a06c6101fa9647fbf343a
SrBottine <lucasbottine7@gmail.com>
2022-08-29 19:24:26 -0300

    first commit

---
+++ aula 2/script.js
@@ -0,0 +1,30 @@
+/*var vouf = false;
+console.log(typeof(vouf));
+
+var num = 1;
+console.log(typeof(num));
+
+var nome = 'diana';
+console.log(typeof(nome)); */
+
+/*var variavel = 'carro';
+variavel = 'uber';
+console.log(variavel); */
+
+/*let variavel = 'carro';
+variavel = 'uber';
+console.log(variavel);*/
+
+/*const variavel = 'pinto';
+console.log(variavel);*/
+
+/*var escopoglobal = 'global';
+console.log(escopoglobal);
+
+function escopoglocal() {
+    let escopoglocalinterno = 'local';
+    console.log(escopoglocalinterno);
+}
+
+escopoglocal();*/
+
