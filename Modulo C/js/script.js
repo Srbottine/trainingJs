a41e4678635dee42a1d89a5276933dacfff17639
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-04-11 21:11:52 +0000

    primeiro testes em DOM

---
+++ Modulo C/js/script.js
@@ -0,0 +1,14 @@
+var p1 = window.document.getElementsByTagName('p')[1]
+window.document.write('esta escrito assim:  '  +p1.innerHTML)
+
+var corpo = window.document.body
+var p1 = window.document.getElementsByTagName('p')[1]
+/*
+var d = window.document.getElementById('msg')
+d.style.background = 'green'
+d.innerText = 'estou aguardando...'
+var c = window.document.getElementsByName('msg')[0]
+c.innerText = 'ola'
+*/
+var a = window.document.querySelector('div#msg')
+a.style.background = 'blue'
