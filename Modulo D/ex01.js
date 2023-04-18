cf7cafa0c8b33778cc5d8615dd0e42e8d83fb934
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-04-18 21:55:30 +0000

    D module

---
+++ Modulo D/ex01.js
@@ -0,0 +1,5 @@
+var vel = 60.5
+console.log(`a velocidade do seu carro é ${vel}`)
+if( vel > 60.5) {
+    console.log(`voce foi multado`)
+}
---
+++ Modulo D/ex02.js
@@ -0,0 +1,7 @@
+var pais = 'Brasil'
+if(pais == Brasil){
+    console.log(`voce e brasileiro`)
+}else{
+    console.log(`voce e estrangerio
+    `)
+}
