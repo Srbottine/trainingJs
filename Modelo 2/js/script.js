9d1158d8b8ab10b7441d490935f9663ce267b134
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-04-20 21:59:55 +0000

    full exercise

--- Modelo 2/js/script.js
+++ Modelo 2/js/script.js
@@ -1,27 +1,29 @@
-function check() {
+var Bcheck = document.querySelector('input#Bcheck')
+Bcheck.addEventListener('click', check)
 
+function check() {
     var date = new Date()
     var year = date.getFullYear()
-    var txtyear = document.querySelector('input#txtano')
+    var fyear = document.querySelector('input#txtano')
     var res = document.querySelector('div#res')
 
-    if (txtyear.value.length == 0 || Number(txtyear.value) > year) {
-
-        window.alert('[ERRO]ano invalido')
+    if (fyear.value.length == 0 || fyear.value > year) {
+        window.alert(`[ERRO] data errada`)
 
     } else {
-
         var fsex = document.getElementsByName('radsex')
-        var age = year - Number(txtyear.value)
+        var fage = year - Number(fyear.value)
         var genery = ''
-        
+
         if (fsex[0].checked) {
-            genery = 'homem'  
+            genery = 'Homem'
 
         } else if (fsex[1].checked) {
-            genery = 'mulher'  
-
+            genery = 'Mulher'
         }
-       
-    }
+        
+        res.innerHTML = `${genery}, ${fage}`
+
+    } 
+
 }
