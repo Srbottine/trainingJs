011eda7d52d941e07610b1dad27560fbe49beb8a
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-04-25 22:05:24 +0000

    Complete arrays

---
+++ Modulo F/ex09.js
@@ -0,0 +1,17 @@
+let num = [5, 8 , 9 , 3]
+/*
+for (let pos = 0; pos < num.length; pos++){
+    console.log(num[pos])
+}
+*/
+for (let pos in num) {
+    console.log(num[pos])
+}
+/*
+num.push(7)
+num.sort()
+
+console.log(num)
+console.log(num.length)
+console.log(num[0])
+*/
