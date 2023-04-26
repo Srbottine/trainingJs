5107b027bbe094a79b2280645db5d20fa7afea72
Srbottine <45329014+Srbottine@users.noreply.github.com>
2023-04-26 19:43:40 +0000

    a

---
+++ Modulo F/ex10.js
@@ -0,0 +1,16 @@
+/*
+function parimpar(n){
+    if (n%2 ==0){
+        return 'par'
+    } else {
+        return 'impar'
+    }
+
+}
+console.log(parimpar(4))
+*/
+let v = function(x) {
+    return x*2
+}
+
+console.log(v(5))
